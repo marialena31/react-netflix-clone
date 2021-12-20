28ac97c3067533dc10045b7713e7e4f2ef8b177b
Maria'lena Pietri <marialena.pietri@maps-system.com>
2021-12-20 22:45:59 +0100

    svg

--- src/components/NetflixApp.exercise.js
+++ src/components/NetflixApp.exercise.js
@@ -5,40 +5,52 @@ import {NetFlixFooter} from './NetFlixFooter'
 import {NetflixHeader} from './NetflixHeader'
 import {getRandomType, getRandomId} from '../utils/helper'
 import {clientApi} from '../utils/clientApi'
-// 🐶 importe les composants MUI
-// import {Alert, AlertTitle} from '@mui/material'
-// import CircularProgress from '@mui/material/CircularProgress';
-// 🐶 importe le Hook 'makeStyles' pour te creer un Hook 'useStyles'
-// import {makeStyles} from '@mui/styles'
+import {Alert, AlertTitle} from '@mui/material'
+import CircularProgress from '@mui/material/CircularProgress';
+import {makeStyles} from '@mui/styles'
 import './Netflix.css'
 
-// 🐶 créé un hook 'useStyles' avec 'makeStyles'
-// 📑 https://material-ui.com/styles/basics/#hook-api
-// Ce hook aura deux classes :
-// 1. alert
-//  width: '50%',
-//  margin : 'auto',
-//  marginBotton:'50px'
-//
-// 2. progress
-//  marginLeft : '30px',
+const useStyles = makeStyles({
+    alert: {
+        width: '50%',
+         margin : 'auto',
+         marginBotton:'50px'
+    },
+    progress: {
+        marginLeft : '30px',
+    },
+});
 
 const NetflixApp = () => {
-  // 🐶 utilise le hook classes ='useStyles', il sera utilié plus bas
+    const myClasses = useStyles();
   const [headerMovie, setHeaderMovie] = React.useState()
   const [type] = React.useState(getRandomType())
   const defaultMovieId = getRandomId(type)
+    const [status, setStatus] = React.useState('idle')
 
-  // 🐶 créé un state 'status', avec la valeur par defaut 'idle'
+
+    const sleep = t => new Promise(resolve => setTimeout(resolve, t))
+
   React.useEffect(() => {
-    // 🐶 changer le status en 'fetching'
+      setStatus('fetching')
+
+      const clientApi = async endpoint => {
+          const page = 1
+          const startChar = endpoint.includes('?') ? `&` : `?`
+          await sleep(2000)
+          const keyLang = `${startChar}api_key=${apiKey}&language=${lang}&page=${page}`
+          return axios.get(`${API_URL}/${endpoint}${keyLang}`)
+      }
     clientApi(`${type}/${defaultMovieId}`)
       .then(response => {
         setHeaderMovie(response)
-        // 🐶 changer le status en 'done'
+          sleep(2000)
+          setStatus('done')
       })
-      // 🐶 changer le status en 'error' dans le catch
-      .catch(error => console.error(error))
+      .catch(error => {
+          console.error(error)
+          setStatus('error')
+      })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
@@ -47,17 +59,12 @@ const NetflixApp = () => {
       <NetflixHeader movie={headerMovie?.data} type={type} />
       <NetflixRow wideImage={false} title="Films Netflix" />
       <NetflixRow wideImage={true} title="Série Netflix" />
-      {/* 🐶 créé une <div> avec le prop 'className' et passer le style 'alert'  
-        - Ajouter ensuite <Alert severity="error"> avec un message d'erreur
-        - conditionnner l'affichage de cette <div> au status === 'error'
-      */}
-
-      {/* 🐶 créé une <div> avec le prop 'className' et passer le style 'progress'  
-        - Ajouter ensuite <CircularProgress />
-        - conditionnner l'affichage de cette <div> au status === 'fetching'
-        - note : modifier la fonction 'clientAPI' avec sleep(2000) pour simuler un long appel
-      */}
-
+        {status === 'error' ? <div className="myClasses.alert">
+            <Alert severity="error">Error message</Alert>
+        </div> : null}
+        {status === 'fetching' ? <div className="myClasses.progress">
+            <CircularProgress />
+        </div> : null}
       <NetFlixFooter />
     </div>
   )
